39e9acb9762a91fccb4565dfb7ba3ad10bf4fee0
Zovin Anggleas <anggleaszovin@gmail.com>
2024-11-08 21:43:42 +1100

    finished profile

--- pricePal/src/pages/Profile/Profile.tsx
+++ pricePal/src/pages/Profile/Profile.tsx
@@ -1,13 +1,29 @@
-import React from "react";
 import './Profile.css';
-import InfoBlock from "../../components/InfoBlock/InfoBlock";
+import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
+import AccountSettings from "../../components/AccountSettings/AccountSettings";
+import PersonalInformation from "../../components/PersonalInformation/PersonalInformation";
 
 const Profile = () => {
   return(
-    <>
+    <div className="profile-flex">
       <div> Profile Page</div>
-      {/* <InfoBlock title="Name" content="Zovin" picture="vite.svg"></InfoBlock> */}
-    </>
+      <div className="account-box">
+
+        <div className="account-header">
+          <text className="account-title">My Account</text>
+          <PrimaryButton label="Sign Out" handler={() =>{alert("Sign Out: TODO dont forget");}}></PrimaryButton>
+        </div>
+
+        <hr/>
+
+        <div className="account-body">
+          <AccountSettings name="Jane Doe" email="jane.doe@example.com" picture="image.png"></AccountSettings>
+          <div className="vertical-line"></div>
+          <PersonalInformation/>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
