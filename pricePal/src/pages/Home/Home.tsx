3d097286c0b88e67f4bba2b6454a8424733ae907
Joshua Sy <z5259375@unsw.edu.au>
2024-11-08 14:08:52 +1100

    secondaryButton

--- pricePal/src/pages/Home/Home.tsx
+++ pricePal/src/pages/Home/Home.tsx
@@ -3,13 +3,16 @@ import './Home.css';
 import PrimaryButton from '../../components/PrimaryButton/PrimaryButton';
 
 const Home = () => {
+  const goToProfilePage = () => {
+    console.log('going to profile page')
+  }
   return(
     <>
       <div> Home Page</div>
       <div className="home-main-container">
-        <h1>Bringing you low prices all day everyday</h1>
+        <h1 className="">Bringing you low prices all day everyday</h1>
         <p>Let us do the work and help you find the best deals on you everyday grocery items</p>
-        <PrimaryButton label={'SHOP NOW'}/>
+        <PrimaryButton label={'SHOP NOW'} handler={goToProfilePage}/>
       </div>
     </>
   )
