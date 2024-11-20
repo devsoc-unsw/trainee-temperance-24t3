235de7f4c7eabb3fcf171c1385d263bba5cdb049
Zovin Anggleas <anggleaszovin@gmail.com>
2024-11-21 04:31:06 +1100

    finished the shopping cart page

--- pricePal/src/App.tsx
+++ pricePal/src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/profile" element={<Profile/>} />
+        <Route path="/cart" element={<Cart/>} />
       </Routes>
     </>
   )
