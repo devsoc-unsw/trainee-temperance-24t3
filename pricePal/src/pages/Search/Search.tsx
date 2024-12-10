5e953e25a9cbaab35d64eecd37de873f65a5959a
Jesslyn <z5455953@ad.unsw.edu.au>
2024-12-10 17:20:43 +0700

    made the favorite page and fixed the card components

--- pricePal/src/pages/Search/Search.tsx
+++ pricePal/src/pages/Search/Search.tsx
@@ -1,13 +1,29 @@
-import ItemSearchList from '../../components/ItemSearchList/ItemSearchList';
+import React, {useState} from "react";
 import './Search.css'
+import ItemSearchList from '../../components/ItemSearchList/ItemSearchList';
+import SortDropDown from "../../components/SortDropDown/SortDropDown";
 
 const Cart = () => {
+    const sortBy = ['Lowest unit price', 'Closest match', 'Price (low to high)', 'Price (high to low)'];
+    const [currSort, setCurrSort] = useState(sortBy[0]);
+    const handleSetCurrSort = (newCurrSort: string) => {
+      setCurrSort(newCurrSort);
+      console.log(newCurrSort);
+    }
+
     return(
         <div className='profile-flex'>
             <div> search page </div>
             <div className="item-search-box">
                 <div className="item-search-header">
                     <text className="account-title">Showing search for "bwfgregewg"</text>
+                </div>
+
+                <div className="custom-divider"></div>  {/* Styled div for the line */}
+
+                <div id='search-products-filter-sort'>
+                    <h2 style={{'marginRight': '10px'}}>Sort by:</h2>
+                    <SortDropDown dropDownList={sortBy} currSelected={currSort} setCurrSelected={handleSetCurrSort}/>
                 </div>
 
                 <div className="item-search-body">
