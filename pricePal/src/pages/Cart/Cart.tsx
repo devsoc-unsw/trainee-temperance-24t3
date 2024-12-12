2a89934c19ad2b9926103c4974d72ea4facd65b8
Jesslyn <z5455953@ad.unsw.edu.au>
2024-12-12 21:48:57 +0700

    added header bar to every page

--- pricePal/src/pages/Cart/Cart.tsx
+++ pricePal/src/pages/Cart/Cart.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ShoppingCartList from '../../components/ShoppingCartList/ShoppingCartList';
+import Header from "../../components/Header/Header";
 import './Cart.css'
 
 const Cart = () => {
@@ -10,21 +11,24 @@ const Cart = () => {
     const total = colesSubtotal + wooliesSubtotal;
 
     return(
-        <div className='profile-flex'>
-            <div> Shopping Cart </div>
-            <div className="shopping-cart-box">
-                <div className="account-header">
-                    <text className="account-title">Shopping Cart</text>
-                </div>
+        <>
+            <Header />
+            <div className='profile-flex'>
+                {/* <div> Shopping Cart </div> */}
+                <div className="shopping-cart-box">
+                    <div className="account-header">
+                        <text className="account-title">Shopping Cart</text>
+                    </div>
 
-                <div className="shopping-cart-body">
-                    <ShoppingCartList headerType='coles-header' shop='Coles' subtotal={colesSubtotal} onSubtotalChange={(subtotal: number) => setColesSubtotal(subtotal)}></ShoppingCartList>
-                    {/* <div className="cart-divider"></div> */}
-                    <ShoppingCartList headerType='woolies-header' shop="Woolsworth" subtotal={wooliesSubtotal} onSubtotalChange={(subtotal) => setWooliesSubtotal(subtotal)}></ShoppingCartList>
+                    <div className="shopping-cart-body">
+                        <ShoppingCartList headerType='coles-header' shop='Coles' subtotal={colesSubtotal} onSubtotalChange={(subtotal: number) => setColesSubtotal(subtotal)}></ShoppingCartList>
+                        {/* <div className="cart-divider"></div> */}
+                        <ShoppingCartList headerType='woolies-header' shop="Woolsworth" subtotal={wooliesSubtotal} onSubtotalChange={(subtotal) => setWooliesSubtotal(subtotal)}></ShoppingCartList>
+                    </div>
+                    <div className="shopping-cart-total shopping-cart-text">Total: ${total.toFixed(2)}</div>
                 </div>
-                <div className="shopping-cart-text">Total: ${total.toFixed(2)}</div>
             </div>
-        </div>
+        </>
     )
 }
 
--- pricePal/src/pages/Favorites/Favorites.tsx
+++ pricePal/src/pages/Favorites/Favorites.tsx
@@ -1,22 +1,26 @@
 import FavoriteItemList from '../../components/FavoriteItemList/FavoriteItemList';
+import Header from "../../components/Header/Header";
 import './Favorites.css'
 
 const Favorites = () => {
     return(
-        <div className='profile-flex'>
-            <div> Favorites page </div>
-            <div className="favorite-item-box">
-                <div className="favorite-header">
-                    <text className="account-title">Favorite Items</text>
-                </div>
+        <>
+            <Header/>
+            <div className='profile-flex'>
+                {/* <div> Favorites page </div> */}
+                <div className="favorite-item-box">
+                    <div className="favorite-header">
+                        <text className="account-title">Favorite Items</text>
+                    </div>
 
-                <div className="favorite-item-body">
-                    <FavoriteItemList headerType='coles-header' shop='Coles'></FavoriteItemList>
-                    {/* <div className="cart-divider"></div> */}
-                    <FavoriteItemList headerType='woolies-header' shop="Woolsworth"></FavoriteItemList>
+                    <div className="favorite-item-body">
+                        <FavoriteItemList headerType='coles-header' shop='Coles'></FavoriteItemList>
+                        {/* <div className="cart-divider"></div> */}
+                        <FavoriteItemList headerType='woolies-header' shop="Woolsworth"></FavoriteItemList>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
