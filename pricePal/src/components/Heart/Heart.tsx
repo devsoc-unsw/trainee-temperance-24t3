8f4a6eaac7385cb3c39a11d43f179eafee495eb9
Joshua Sy <z5259375@unsw.edu.au>
2024-12-14 21:06:33 +1100

    commented out heart.tsx for deployment

--- pricePal/src/components/Heart/Heart.tsx
+++ pricePal/src/components/Heart/Heart.tsx
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
-import './Heart.css';
+// import React, { useState } from "react";
+// import './Heart.css';
 
-// DO I SAVE THE FAVORITES HERE IF THE HEART IS CLICKED? SO FOR 
-// EVERY CARD I PASS THE ID OF THE ITEM TO THE HEART COMPONENT?
+// // DO I SAVE THE FAVORITES HERE IF THE HEART IS CLICKED? SO FOR 
+// // EVERY CARD I PASS THE ID OF THE ITEM TO THE HEART COMPONENT?
 
-interface HeartProps {
-  handler: MouseEventHandler;
-}
+// interface HeartProps {
+//   handler: MouseEventHandler;
+// }
 
-const Heart = ({handler}: HeartProps) => {
-  // Set state of heart to either hearted or unhearted.
-  const [isHearted, setIsHearted] = useState(false);
+// const Heart = ({handler}: HeartProps) => {
+//   // Set state of heart to either hearted or unhearted.
+//   const [isHearted, setIsHearted] = useState(false);
 
-  // Function to toggle the heart.
-  const toggleHeart = () => {
-    setIsHearted(!isHearted);
-    handler();
-  }
+//   // Function to toggle the heart.
+//   const toggleHeart = () => {
+//     setIsHearted(!isHearted);
+//     handler();
+//   }
 
-  return(
-    <>
-      <button className="heart" onClick={toggleHeart}>
-        <img 
-          className="heart-image"
-          src={isHearted ? 'hearted.png' : 'unhearted.png'} 
-          alt={isHearted ? 'Hearted' : 'Unhearted'} 
-        />
-      </button>
-    </>
-  )
-}
-export default Heart
+//   return(
+//     <>
+//       <button className="heart" onClick={toggleHeart}>
+//         <img 
+//           className="heart-image"
+//           src={isHearted ? 'hearted.png' : 'unhearted.png'} 
+//           alt={isHearted ? 'Hearted' : 'Unhearted'} 
+//         />
+//       </button>
+//     </>
+//   )
+// }
+// export default Heart
